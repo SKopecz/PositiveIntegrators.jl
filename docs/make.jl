f64c925f8db1624bc2e24bd9547c8054fe8bb0c3
Hendrik Ranocha <mail@ranocha.de>
2023-10-20 16:59:36 +0200

    set up template for docs

---
+++ docs/make.jl
@@ -0,0 +1,75 @@
+using Documenter
+using Pkg: Pkg
+using PositiveIntegrators
+
+# Define module-wide setups such that the respective modules are available in doctests
+DocMeta.setdocmeta!(PositiveIntegrators,
+  :DocTestSetup, :(using PositiveIntegrators); recursive=true)
+
+# Copy some files from the top level directory to the docs and modify them
+# as necessary
+open(joinpath(@__DIR__, "src", "license.md"), "w") do io
+  # Point to source license file
+  println(io, """
+  ```@meta
+  EditURL = "https://github.com/SKopecz/PositiveIntegrators.jl/blob/main/LICENSE.md"
+  ```
+  """)
+  # Write the modified contents
+  println(io, "# License")
+  println(io, "")
+  for line in eachline(joinpath(dirname(@__DIR__), "LICENSE.md"))
+    line = replace(line, "[LICENSE.md](LICENSE.md)" => "[License](@ref)")
+    println(io, "> ", line)
+  end
+end
+
+open(joinpath(@__DIR__, "src", "contributing.md"), "w") do io
+  # Point to source license file
+  println(io, """
+  ```@meta
+  EditURL = "https://github.com/SKopecz/PositiveIntegrators.jl/blob/main/CONTRIBUTING.md"
+  ```
+  """)
+  # Write the modified contents
+  println(io, "# Contributing")
+  println(io, "")
+  for line in eachline(joinpath(dirname(@__DIR__), "CONTRIBUTING.md"))
+    line = replace(line, "[LICENSE.md](LICENSE.md)" => "[License](@ref)")
+    println(io, "> ", line)
+  end
+end
+
+# Make documentation
+makedocs(
+  modules = [PositiveIntegrators],
+  sitename="PositiveIntegrators.jl",
+  format = Documenter.HTML(
+    prettyurls = get(ENV, "CI", nothing) == "true",
+    canonical = "https://SKopecz.github.io/PositiveIntegrators.jl/stable"
+  ),
+  # Explicitly specify documentation structure
+  pages = [
+    "Home" => "index.md",
+    "Introduction" => "introduction.md",
+    "Tutorials" => [
+      "tutorials/constant_linear_advection.md",
+      "tutorials/advection_diffusion.md",
+      "tutorials/variable_linear_advection.md",
+      "tutorials/wave_equation.md",
+      "tutorials/kdv.md",
+    ],
+    "Automatic differentiation (AD)" => "ad.md",
+    "Applications & references" => "applications.md",
+    "Benchmarks" => "benchmarks.md",
+    "API reference" => "api_reference.md",
+    "Contributing" => "contributing.md",
+    "License" => "license.md"
+  ]
+)
+
+deploydocs(
+  repo = "github.com/SKopecz/PositiveIntegrators.jl",
+  devbranch = "main",
+  push_preview = true
+)
---
+++ docs/src/index.md
@@ -0,0 +1,59 @@
+# PositiveIntegrators.jl
+
+The [Julia]() library
+[PositiveIntegrators.jl](https://github.com/ranocha/PositiveIntegrators.jl)
+provides several time integration methods developed to preserve the positivity
+of numerical solutions.
+
+TODO: More introduction etc.
+
+
+## Installation
+
+TODO: PositiveIntegrators.jl has to be registered - up to now, it is not!
+
+[PositiveIntegrators.jl](https://github.com/SKopecz/PositiveIntegrators.jl)
+is a registered Julia package. Thus, you can install it from the Julia REPL via
+```julia
+julia> using Pkg; Pkg.add("PositiveIntegrators")
+```
+
+If you want to update PositiveIntegrators.jl, you can use
+```julia
+julia> using Pkg; Pkg.update("PositiveIntegrators")
+```
+As usual, if you want to update PositiveIntegrators.jl and all other
+packages in your current project, you can execute
+```julia
+julia> using Pkg; Pkg.update()
+```
+
+
+## Basic examples
+
+TODO
+
+
+## Referencing
+
+If you use
+[PositiveIntegrators.jl](https://github.com/ranocha/PositiveIntegrators.jl)
+for your research, please cite it using the bibtex entry
+```bibtex
+@misc{PositiveIntegrators.jl,
+  title={{PositiveIntegrators.jl}: {A} {J}ulia library of positivity-perserving
+         time integration methods},
+  author={Kopecz, Stefan and Ranocha, Hendrik and contributors},
+  year={2023},
+  doi={TODO},
+  url={https://github.com/SKopecz/PositiveIntegrators.jl}
+}
+```
+
+
+## License and contributing
+
+This project is licensed under the MIT license (see [License](@ref)).
+Since it is an open-source project, we are very happy to accept contributions
+from the community. Please refer to the section [Contributing](@ref) for more
+details.
