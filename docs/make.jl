50f9ce4255eb890899fdcb0a2fc8b3ef8f64bc8c
Hendrik Ranocha <mail@ranocha.de>
2023-10-21 10:01:14 +0200

    fix license file name

--- docs/make.jl
+++ docs/make.jl
@@ -18,7 +18,7 @@ open(joinpath(@__DIR__, "src", "license.md"), "w") do io
   # Write the modified contents
   println(io, "# License")
   println(io, "")
-  for line in eachline(joinpath(dirname(@__DIR__), "LICENSE.md"))
+  for line in eachline(joinpath(dirname(@__DIR__), "LICENSE"))
     line = replace(line, "[LICENSE.md](LICENSE.md)" => "[License](@ref)")
     println(io, "> ", line)
   end
