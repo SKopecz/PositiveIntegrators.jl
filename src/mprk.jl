12e41a56e7d22d84a53770b0f155a850fe5d3899
Stefan Kopecz <kopecz@mathematik.uni-kassel.de>
2024-05-08 16:34:10 +0200

    Improvements after review
    -removed code for in-place version of MPRK43
    
    -added doc string for MPRK43II
    
    - corrected typos

--- src/mprk.jl
+++ src/mprk.jl
@@ -600,7 +600,7 @@ function perform_step!(integrator, cache::MPRK22ConstantCache, repeat_step = fal
     Ptmp = a21 * P
     integrator.stats.nf += 1
 
-    # avoid division by zero due to zero patankar weights
+    # avoid division by zero due to zero Patankar weights
     σ = add_small_constant(uprev, small_constant)
 
     # build linear system matrix
@@ -621,7 +621,7 @@ function perform_step!(integrator, cache::MPRK22ConstantCache, repeat_step = fal
         # σ = σ .* (u ./ σ) .^ (1 / a21) # generated Infs when solving brusselator
         σ = σ .^ (1 - 1 / a21) .* u .^ (1 / a21)
     end
-    # avoid division by zero due to zero patankar weights
+    # avoid division by zero due to zero Patankar weights
     σ = add_small_constant(σ, small_constant)
 
     P2 = f.p(u, p, t + a21 * dt)
@@ -724,7 +724,7 @@ as keyword argument `linsolve`.
 - Stefan Kopecz and Andreas Meister.
   "Unconditionally positive and conservative third order modified Patankar–Runge–Kutta 
    discretizations of production–destruction systems."
-   BIT Numer Math 58 (2018): 691–728.
+   BIT Numerical Mathematics 58 (2018): 691–728.
   [DOI: 10.1007/s10543-018-0705-1](https://doi.org/10.1007/s10543-018-0705-1)
 """
 struct MPRK43I{T, Thread, F} <: OrdinaryDiffEqAdaptiveAlgorithm
@@ -734,15 +734,38 @@ struct MPRK43I{T, Thread, F} <: OrdinaryDiffEqAdaptiveAlgorithm
     linsolve::F
 end
 
+function MPRK43I(alpha, beta; thread = False(), linsolve = LUFactorization())
+    MPRK43I{typeof(alpha), typeof(thread), typeof(linsolve)}(alpha, beta,
+                                                             thread, linsolve)
+end
+
+"""
+    MPRK43II(γ; [linsolve = ...])
+
+A third-order modified Patankar-Runge-Kutta algorithm for (conservative)
+production-destruction systems. This one-step, four-stage method is
+third-order accurate, unconditionally positivity-preserving, and linearly
+implicit. The parameters `γ` is described by Kopecz and Meister (2018).
+
+This modified Patankar-Runge-Kutta method requires the special structure of a
+[`PDSProblem`](@ref) or a [`ConservativePDSProblem`](@ref).
+
+You can optionally choose the linear solver to be used by passing an
+algorithm from [LinearSolve.jl](https://github.com/SciML/LinearSolve.jl)
+as keyword argument `linsolve`.
+
+## References
+
+- Stefan Kopecz and Andreas Meister.
+  "Unconditionally positive and conservative third order modified Patankar–Runge–Kutta 
+   discretizations of production–destruction systems."
+   BIT Numerical Mathematics 58 (2018): 691–728.
+  [DOI: 10.1007/s10543-018-0705-1](https://doi.org/10.1007/s10543-018-0705-1)
+"""
 struct MPRK43II{T, Thread, F} <: OrdinaryDiffEqAdaptiveAlgorithm
     gamma::T
     thread::Thread
     linsolve::F
-end
-
-function MPRK43I(alpha, beta; thread = False(), linsolve = LUFactorization())
-    MPRK43I{typeof(alpha), typeof(thread), typeof(linsolve)}(alpha, beta,
-                                                             thread, linsolve)
 end
 
 function MPRK43II(gamma; thread = False(), linsolve = LUFactorization())
@@ -827,39 +850,6 @@ function set_constant_parameters(alg::MPRK43II)
     return a21, a31, a32, b1, b2, b3, c2, c3, beta1, beta2, q1, q2
 end
 
-function alg_cache(alg::Union{MPRK43I, MPRK43II}, u, rate_prototype, ::Type{uEltypeNoUnits},
-                   ::Type{uBottomEltypeNoUnits}, ::Type{tTypeNoUnits},
-                   uprev, uprev2, f, t, dt, reltol, p, calck,
-                   ::Val{true}) where {uEltypeNoUnits, uBottomEltypeNoUnits, tTypeNoUnits}
-    a21, a31, a32, b1, b2, b3, c2, c3, beta1, beta2, q1, q2 = set_constant_parameters(alg)
-    tab = MPRK43ConstantCache(a21, a31, a32, b1, b2, b3, c2, c3,
-                              beta1, beta2, q1, q2, floatmin(uEltypeNoUnits))
-
-    tmp = zero(u)
-
-    M = p_prototype(u, f)
-    linsolve_tmp = zero(u)
-    weight = similar(u, uEltypeNoUnits)
-    recursivefill!(weight, false)
-
-    linprob = LinearProblem(M, _vec(linsolve_tmp); u0 = _vec(tmp))
-    linsolve = init(linprob, alg.linsolve, alias_A = true, alias_b = true,
-                    assumptions = LinearSolve.OperatorAssumptions(true))
-
-    MPRK43Cache(u, uprev, tmp,
-                zero(u), # atmp
-                zero(rate_prototype), # k
-                zero(rate_prototype), #fsalfirst
-                p_prototype(u, f), # P
-                p_prototype(u, f), # P2
-                zero(u), # D
-                zero(u), # D2
-                M,
-                zero(u), # σ
-                tab, alg.thread,
-                linsolve_tmp, linsolve, weight)
-end
-
 struct MPRK43ConstantCache{T} <: OrdinaryDiffEqConstantCache
     a21::T
     a31::T
@@ -919,7 +909,7 @@ function perform_step!(integrator, cache::MPRK43ConstantCache, repeat_step = fal
     Ptmp = a21 * P
     integrator.stats.nf += 1
 
-    # avoid division by zero due to zero patankar weights
+    # avoid division by zero due to zero Patankar weights
     σ = add_small_constant(uprev, small_constant)
     σ0 = σ
 
@@ -938,7 +928,7 @@ function perform_step!(integrator, cache::MPRK43ConstantCache, repeat_step = fal
     # compute Patankar weight denominator
     σ = σ .^ (1 - q1) .* u .^ q1
 
-    # avoid division by zero due to zero patankar weights
+    # avoid division by zero due to zero Patankar weights
     σ = add_small_constant(σ, small_constant)
 
     P2 = f.p(u, p, t + c2 * dt)
@@ -960,7 +950,7 @@ function perform_step!(integrator, cache::MPRK43ConstantCache, repeat_step = fal
     if !(q1 ≈ q2)
         σ = σ0 .^ (1 - q2) .* u2 .^ q2
 
-        # avoid division by zero due to zero patankar weights
+        # avoid division by zero due to zero Patankar weights
         σ = add_small_constant(σ, small_constant)
     end
 
@@ -978,7 +968,7 @@ function perform_step!(integrator, cache::MPRK43ConstantCache, repeat_step = fal
     σ = sol.u
     integrator.stats.nsolve += 1
 
-    # avoid division by zero due to zero patankar weights
+    # avoid division by zero due to zero Patankar weights
     σ = add_small_constant(σ, small_constant)
 
     P3 = f.p(u, p, t + c2 * dt)
@@ -1005,59 +995,6 @@ function perform_step!(integrator, cache::MPRK43ConstantCache, repeat_step = fal
     integrator.EEst = integrator.opts.internalnorm(atmp, t)
 
     integrator.u = u
-end
-
-function initialize!(integrator, cache::MPRK43Cache)
-    @unpack k, fsalfirst = cache
-    integrator.fsalfirst = fsalfirst
-    integrator.fsallast = k
-    integrator.kshortsize = 1
-    resize!(integrator.k, integrator.kshortsize)
-    integrator.k[1] = integrator.fsalfirst
-end
-
-function perform_step!(integrator, cache::MPRK43Cache, repeat_step = false)
-    @unpack t, dt, uprev, u, f, p = integrator
-    @unpack tmp, atmp, P, P2, D, D2, M, σ, thread, weight = cache
-    @unpack a21, a31, a32, b1, b2, b3, c2, c3, beta1, beta2, q1, q2, small_constant = cache.tab
-
-    uprev .= uprev .+ small_constant
-
-    f.p(P, uprev, p, t) # evaluate production terms
-    sum_destruction_terms!(D, P) # store destruction terms in D
-    integrator.stats.nf += 1
-
-    build_mprk_matrix!(M, a21, P, D, uprev, dt)
-    # Same as linres = M \ uprev
-    linres = dolinsolve(integrator, cache.linsolve;
-                        A = M, b = _vec(uprev),
-                        du = integrator.fsalfirst, u = u, p = p, t = t,
-                        weight = weight)
-    u .= linres
-    integrator.stats.nsolve += 1
-
-    u .= u .+ small_constant
-
-    σ .= uprev .* (u ./ uprev) .^ (1 / a21) .+ small_constant
-
-    f.p(P2, u, p, t + a21 * dt) # evaluate production terms
-    sum_destruction_terms!(D, P) # store destruction terms in D
-    sum_destruction_terms!(D2, P2) # store destruction terms in D2
-
-    build_mprk_matrix!(M, b1, P, D, b2, P2, D2, σ, dt)
-    # Same as linres = M \ uprev
-    linres = dolinsolve(integrator, cache.linsolve;
-                        A = M, b = _vec(uprev),
-                        du = integrator.fsalfirst, u = u, p = p, t = t,
-                        weight = weight)
-    u .= linres
-    integrator.stats.nsolve += 1
-
-    tmp .= u .- σ
-    calculate_residuals!(atmp, tmp, uprev, u, integrator.opts.abstol,
-                         integrator.opts.reltol, integrator.opts.internalnorm, t,
-                         thread)
-    integrator.EEst = integrator.opts.internalnorm(atmp, t)
 end
 
 ########################################################################################
