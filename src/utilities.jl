ace698d6f8a4358662726f7954911471c79cc36d
Stefan Kopecz <kopecz@mathematik.uni-kassel.de>
2024-10-02 15:33:40 +0200

    bugfix

--- src/utilities.jl
+++ src/utilities.jl
@@ -135,7 +135,6 @@ end
 Adds work-precision data to the dictionary `dict`, which was created with `work_precion_fixed`.
 See [`work_precision_fixed`](@ref) for the meaning of the inputs.
 """
-# functions to compute data for work_precision diagrams
 function work_precision_fixed!(dict, prob, algs, labels, dts, alg_ref;
                                compute_error = rel_max_error_tend, seconds = 2,
                                numruns = 20)
