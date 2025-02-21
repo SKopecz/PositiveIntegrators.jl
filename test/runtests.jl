961b404afc3ba757e9923fee2e909bc33ba90427
Joshua Lampert <joshua.lampert@uni-hamburg.de>
2025-02-21 19:21:22 +0100

    revert using ADTypes

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,7 +19,7 @@ using ExplicitImports: check_no_implicit_imports, check_no_stale_explicit_import
     experimental_orders_of_convergence(prob, alg, dts;
                                       test_time = nothing,
                                       only_first_index = false,
-                                      ref_alg = TRBDF2(autodiff = AutoFiniteDiff()))
+                                      ref_alg = TRBDF2(autodiff = false))
 
 Solve `prob` with `alg` and fixed time steps taken from `dts`, and compute
 the errors at `test_time`. If`test_time` is not specified the error is computed
@@ -32,7 +32,7 @@ solution is computed using `ref_alg`.
 """
 function experimental_orders_of_convergence(prob, alg, dts; test_time = nothing,
                                             only_first_index = false,
-                                            ref_alg = TRBDF2(autodiff = AutoFiniteDiff()))
+                                            ref_alg = TRBDF2(autodiff = false))
     @assert length(dts) > 1
     errors = zeros(eltype(dts), length(dts))
 
@@ -929,11 +929,11 @@ end
             end
 
             # non-stiff conservative problems (in-place)
-            # Requires autodiff = AutoFiniteDiff()
+            # Requires autodiff = false
             probs = (prob_pds_linmod_inplace,)
-            algs = (Euler(), ImplicitEuler(autodiff = AutoFiniteDiff()), Tsit5(),
-                    Rosenbrock23(autodiff = AutoFiniteDiff()), SDIRK2(autodiff = AutoFiniteDiff()),
-                    TRBDF2(autodiff = AutoFiniteDiff()))
+            algs = (Euler(), ImplicitEuler(autodiff = false), Tsit5(),
+                    Rosenbrock23(autodiff = false), SDIRK2(autodiff = false),
+                    TRBDF2(autodiff = false))
             @testset "$alg" for prob in probs, alg in algs
                 dt = (last(prob.tspan) - first(prob.tspan)) / 1e4
                 sol = solve(prob, alg; dt, isoutofdomain = isnegative) # use explicit f
@@ -1138,7 +1138,7 @@ end
             dt = 0.25
             sol_MPE_op = solve(prob_op, MPE(); dt)
             sol_MPE_op_2 = solve(prob_op_2, MPE(); dt)
-            sol_IE_op = solve(prob_op, ImplicitEuler(autodiff = AutoFiniteDiff());
+            sol_IE_op = solve(prob_op, ImplicitEuler(autodiff = false);
                               dt, adaptive = false)
             @test sol_MPE_op.t ≈ sol_MPE_op_2.t ≈ sol_IE_op.t
             @test sol_MPE_op.u ≈ sol_MPE_op_2.u ≈ sol_IE_op.u
@@ -1161,7 +1161,7 @@ end
             dt = 0.25
             sol_MPE_ip = solve(prob_ip, MPE(); dt)
             sol_MPE_ip_2 = solve(prob_ip_2, MPE(); dt)
-            sol_IE_ip = solve(prob_ip, ImplicitEuler(autodiff = AutoFiniteDiff());
+            sol_IE_ip = solve(prob_ip, ImplicitEuler(autodiff = false);
                               dt, adaptive = false)
             @test sol_MPE_ip.t ≈ sol_MPE_ip_2.t ≈ sol_IE_ip.t
             @test sol_MPE_ip.u ≈ sol_MPE_ip_2.u ≈ sol_IE_ip.u
